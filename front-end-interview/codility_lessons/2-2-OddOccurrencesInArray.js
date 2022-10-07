348d3a89d4725ca927ead9af27e27c65a31c086b
pac <sunpochin@gmail.com>
2022-10-07 22:35:51 +0800

    test double quote boolean

--- front-end-interview/codility_lessons/2-2-OddOccurrencesInArray.js
+++ front-end-interview/codility_lessons/2-2-OddOccurrencesInArray.js
@@ -4,22 +4,28 @@
 //  O(N) or O(N*log(N)) 
 
 function solution(A) {
-	// write your code in JavaScript (Node.js 8.9.4)
-  const mySet = new Set();
-  for (let i = 0; i < A.length; i++) {
-    
-    if (mySet.has(A[i]) ) {
-      mySet.delete(A[i]);
-    } else {
-      mySet.add(A[i]);
-    }
-  }
-  // console.log('result: ', result);
-  const iterator1 = mySet.values();
-  let answer = iterator1.next().value;
-  return answer;
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/values
+	// https://pjchender.dev/javascript/js-set/
+	const mySet = new Set();
+	for (let i = 0; i < A.length; i++) {
+		if (mySet.has(A[i])) {
+			mySet.delete(A[i]);
+		} else {
+			mySet.add(A[i]);
+		}
+	}
+	// console.log('result: ', result);
+	const iterator1 = mySet.values();
+	let answer = iterator1.next().value;
+	return answer;
 }
 
 let A = [9, 3, 9, 3, 9, 7, 9];
 let ret = solution(A);
 console.log('ret: ', ret)
+
+if ("") {
+	console.log('yes');
+} else {
+	console.log('no')
+}
