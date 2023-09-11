eca44b76a294738f02a5dc3ca380caa4dc3ca031
pac <sunpochin@gmail.com>
2023-09-12 00:30:40 +0800

    update promise exercise

--- front-end-interview/3closure.js
+++ front-end-interview/3closure.js
@@ -13,18 +13,37 @@ function catHouse() {
     count += 1;
     console.log(count + ' cats');
   }
-  return countCats;
+  return countCats
 }
 
-const countDogs = dogHouse();
-const countCats = catHouse();
+function createCounter(name) {
+  var count = 0;
+  function countAnimal() {
+    count += 1;
+    console.log(count + ' ' + name);
+  }
+  return countAnimal;
+}
 
-countDogs(); // "1 dogs"
-countDogs(); // "2 dogs"
-countDogs(); // "3 dogs"
+const countDogs = createCounter('dogs');
+const countCats = createCounter('cats');
+countDogs();
+countDogs();
+countDogs();
+countCats();
+countCats();
+countDogs();
 
-countCats(); // "1 cats"
-countCats(); // "2 cats"
+// const countDogs = dogHouse();
+// const countCats = catHouse();
 
-countDogs(); // "4 dogs"
+// countDogs(); // "1 dogs"
+// countDogs(); // "2 dogs"
+// countDogs(); // "3 dogs"
+
+// countCats(); // "1 cats"
+// countCats(); // "2 cats"
+
+// countDogs(); // "4 dogs"
+
 
